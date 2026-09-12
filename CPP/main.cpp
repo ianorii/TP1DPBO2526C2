f98100e1a1e80db494847d530cdd21f77a677bc9
rian <rian22anugrah@gmail.com>
2026-09-12 14:00:28 +0700

    feat: add fitur delete

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -1,24 +1,27 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include<cmath>
 #include "Bioskop.cpp"
 
 using namespace std;
 vector<Bioskop> dataBioskop;
+int col[5] = {0};
 
 void intro() {
     cout << "-----------------------BIOSKOP-----------------------\n";
 }
 
+// fungsi memeriksa apakah ID sudah ada
 bool searchId(int target) {
-    int found = false;
-    int i = 0;
-    while(!found && i < dataBioskop.size()) {
-        if(dataBioskop[i].getId() == target) found = true;
-        else i++;
+    for(auto data : dataBioskop) {
+        if(data.getId() == target) {
+            // jika id sudah ada
+            return true;
+        }
     }
-
-    return found;
+    // mengembalikan false jika id belum ada
+    return false;
 }
 
 bool isInteger(string s) {
@@ -30,51 +33,112 @@ bool isInteger(string s) {
     return true;
 }
 
-void insert() {
-    cout << "---------------Masukan Data Bioskop---------------\n";
-    
-    // error handling ID
+bool isFloat(string s) {
+    if(s.empty()) return false;
+    if(s[0] == '.' || s[0] == '-') return false;
+
+    int flag = 0;
+    for(int i = 0; i < s.length(); i++) {
+        if((s[i] < '0' || s[i] > '9') && s[i] != '.') return false;
+        if(s[i] == '.' && flag > 0) return false;
+        else flag++;
+    }
+    return true;
+}
+
+int inputInteger(string message, int type) {
     string input;
-    int id, flag;
+    int flag;
+    int ans;
+
     do {
         flag = 1;
-        cout << "Masukan ID : ";
+        cout << message;
         getline(cin, input);
         while(!isInteger(input)) {
             cout << "inputan cuma boleh bilangan bulat yeah\n";
-            cout << "Masukan ID : ";
+            cout << message;
             getline(cin, input);
         }
-        id = stoi(input);
         
-        if(id <= 0) {
-            cout << "id harus lebih dari 0\n";
+        ans = stoi(input);
+        if(ans <= 0) {
+            cout << "nilainya harus lebih dari 0\n";
             flag = 0;
-        } else if(searchId(id)) {
+        } else if(type == 1 && searchId(ans)) {
             cout << "udah ada id nya kocak\n";
             flag = 0;
         }
     } while(!flag);
 
-    cout << "Masukan Nama Bioskop : ";
-    string name; cin >> name;
+    return ans;
+}
 
-    cout << "Masukan Alamat Bioskop : ";
-    cin.ignore();   // hapus buffer \n
-    string address; getline(cin, address);
+float inputFloat(string message) {
+    string input;
+    float ans;
+    int flag;
 
-    cout << "Masukan Jumlah Studio : ";
-    int totalStudios; cin >> totalStudios;
+    do {
+        flag = 1;
+        cout << message;
+        getline(cin, input);
+        
+        while(!isFloat(input)) {
+            cout << "inputan tidak sesuai format\n";
+            cout << message;
+            getline(cin, input);
+        }
 
-    cout << "Masukan Rating Bioskop : ";
-    float rating; cin >> rating;
+        ans = stof(input);
+    } while(!flag);
+
+    return ans;
+}
+
+string inputString(string message) {
+    string ans;
+    cout << message;
+    cin.ignore();
+    getline(cin, ans);
+    return ans;
+}
+
+void insert() {
+    cout << "---------------Masukan Data Bioskop---------------\n";
+    
+    int id = inputInteger("Masukan ID : ", 1);
+    string name = inputString("Masukan Nama Bioskop : ");
+    string address = inputString("Masukan Alamat Bioskop : ");
+    int totalStudios = inputInteger("Masukan Jumlah Studio : ", 0);    
+    float rating = inputFloat("Masukan Rating Bioskop : ");
 
     Bioskop now = Bioskop(id, name, address, totalStudios, rating);
     dataBioskop.push_back(now);
 }
 
+void del() {
+    cout << "---------------Hapus Data Bioskop---------------\n";
+    int id = inputInteger("Masukan ID Bioskop : ", 0);
+
+    int i = 0;
+    for(auto data : dataBioskop) {
+        if(data.getId() == id) {
+            dataBioskop.erase(dataBioskop.begin() + i);
+            return;
+        }
+        else i++;
+    }
+
+    cout << "ID Bioskop tidak ditemukan\n";
+}
+
+void show() {
+}
+
 int main() {
     intro();
+    cout << col[0] << endl;
 
     while(true) {
         cout << "Pilih Opsi : " << endl;
@@ -85,13 +149,13 @@ int main() {
         switch(option) {
             case 1:
                 insert();
-                cout << dataBioskop[0].getAddress() << endl;
                 break;
             case 2:
                 break;
             case 3:
                 break;
             case 4:
+                del();
                 break;
             case 5:
                 break;
