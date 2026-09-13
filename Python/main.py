83cf44747a756b15023c8e3de9fb847d99e17721
rian <rian22anugrah@gmail.com>
2026-09-13 21:39:36 +0700

    feat: add fitur insert

--- Python/main.py
+++ Python/main.py
@@ -1,4 +1,5 @@
 from Bioskop import Bioskop
+from tabulate import tabulate
 
 RESET ="\033[0m"
 RED ="\033[31m"
@@ -6,6 +7,8 @@ GREEN ="\033[32m"
 CYAN ="\033[36m"
 YELLOW ="\033[33m"
 BOLD ="\033[1m"
+
+dataBioskop = []
 
 def intro():
     print(BOLD + CYAN)
@@ -24,6 +27,43 @@ def intro():
     print("+====================================================+")
     print(RESET)
 
+def searchId(target:int):
+    for data in dataBioskop:
+        if data.getId() == target : return True
+    return False
+
+def inputInteger(message:str):
+    while True:
+        ans = input(message)
+        try: ans = int(ans)
+        except ValueError:
+            print("masukin angka aja")
+            continue
+        return ans
+
+def inputFloat(message:str):
+    while True:
+        ans = input(message)
+        try: ans = float(ans)
+        except ValueError:
+            print("ini bukan bilangan float")
+            continue
+        return ans
+
+def insert():
+    while True:
+        id = inputInteger("masukan ID : ")
+        if searchId(id): print("data ID sudah ada")
+        else: break
+
+    name = str(input("masukan nama bioskop : "))
+    address = str(input("masukan alamat bioskop : "))
+    totalStudios = inputInteger("masukan total studio : ")
+    rating = inputFloat("masukan rating : ")
+
+    now = Bioskop(id, name, address, totalStudios, rating)
+    dataBioskop.append(now)
+
 def main():
     intro()
 
@@ -31,9 +71,21 @@ def main():
     while not exit:
         option = int(input(BOLD + "  Pilih opsi [1-6] >> " + RESET))
 
-        if option == 6:
-            print(GREEN + "\n  Sampai jumpa! Terima kasih telah menggunakan program ini.\n" + RESET);
-            exit = True;
+        match option:
+            case 1: insert()
+            # case 2:
+
+            # case 3:
+
+            # case 4:
+
+            # case 5:
+
+            case 6:
+                print(GREEN + "\n  Sampai jumpa! Terima kasih telah menggunakan program ini.\n" + RESET)
+                exit = True
+            case _:
+                print(RED + "\n  Opsi tidak valid, silakan pilih antara 1 sampai 6." + RESET)
 
 
 if __name__ == "__main__":
